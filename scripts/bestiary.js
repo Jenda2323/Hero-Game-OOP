fa92ab6c1ba72fb92f3f52cad4e00fd7fd647a2d
Jan Fiser <fishus@seznam.cz>
2024-11-24 13:40:10 +0100

    ver. 0.011 opraven banner a statistiky

--- scripts/bestiary.js
+++ scripts/bestiary.js
@@ -1,8 +1,29 @@
 import { Enemy } from "./enemy.js";
 
 export const enemies = {
-  goblin: new Enemy("Skřet", 12, 7, 2, "Zlomený meč", "Skřetí skelet"),
-  skeleton: new Enemy("Kostlivec", 14, 6, 3, "Mrtvolný meč", "Prastaré brnění"),
-  troll: new Enemy("Trol", 25, 10, 3, "Obří kyj", "Kostěné brnění"),
-  darkGhost: new Enemy("Temný duch", 20, 5, 0, "Temný stisk", "Nehmotná aura"),
+  goblin: new Enemy(
+    "Skřet",
+    12,
+    7,
+    2,
+    "Zlomený meč (+6)",
+    "Skřetí skelet (+1)"
+  ),
+  skeleton: new Enemy(
+    "Kostlivec",
+    14,
+    6,
+    3,
+    "Mrtvolný meč (+5)",
+    "Prastaré brnění (+2)"
+  ),
+  troll: new Enemy("Trol", 25, 10, 3, "Obří kyj (+9)", "Kostěné brnění (+2)"),
+  darkGhost: new Enemy(
+    "Temný duch",
+    20,
+    5,
+    0,
+    "Temný stisk (+4)",
+    "Nehmotná aura (0)"
+  ),
 };
